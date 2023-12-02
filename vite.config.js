af8151b172f7f2e4ba1f162d6e5f031d736cb516
Abdirizaq Hathar <cisxaaqo5@gmail.com>
2023-12-02 07:32:09 -0800

    Updated vite configuration settings

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 
   // If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/, for example your repository is at https://github.com/<USERNAME>/<REPO_NAME>, then set base to '/<REPO_NAME>/'.
 
-  base: '/gitprofile/',
+  base: '/App4080-Project/',
   plugins: [
     react(),
     VitePWA({
